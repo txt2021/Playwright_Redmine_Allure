6d90a3700a358590fccc700a275edee738bd42cf
Tetiana Chuiasova <txt2021@gmail.com>
2022-09-28 13:42:46 +0300

    add report

--- playwright.config.js
+++ playwright.config.js
@@ -17,7 +17,7 @@ const config = {
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
- // reporter: 'html'
+  reporter: "allure-playwright",
   use: {
     headless: false,
     viewport: { width: 1280, height: 720 },
